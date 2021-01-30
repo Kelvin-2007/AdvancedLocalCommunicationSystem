877c6437b3709da0ec9eb7a23fa5970b17184ed8
Kelvin-2007 <72763850+Kelvin-2007@users.noreply.github.com>
2021-01-31 03:07:38 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+from ALCS import IPv4Address, IPv6Address, Entity
+
+ip1 = IPv4Address()
+ip2 = IPv4Address()
+
+# IPAddress are basically strings so you play around with them
+
+object1 = Entity(ip1)
+object2 = Entity(ip2)
+
+object1.Transmit(object2, "Hello World!")
+print(object2.Receive(object1))
+
+# you can also stuff Entity inside list so you can put a bunch of Entity inside a list and you access can it by Bunch[index].Transmit whatever you wnat to do
